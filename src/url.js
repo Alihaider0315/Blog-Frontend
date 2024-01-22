e2d142c4e8e95d77af746ebcc7f8a9738c7c0168
Ali Haider <ah712200@Gmail.com>
2024-01-23 01:28:09 +0500

    Url updated

--- src/url.js
+++ src/url.js
@@ -1,2 +1,2 @@
-export const URL = "https://tricky-red-pea-coat.cyclic.app"
-export const IF = "https://tricky-red-pea-coat.cyclic.app/images/"
+export const URL = "http://localhost:5000"
+export const IF = "http://localhost:5000/images/"
