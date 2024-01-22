0a0634eb71d9debeab2d78d350c73c30d4646849
Ali Haider <ah712200@Gmail.com>
2024-01-23 02:02:15 +0500

    Update url.js

--- src/url.js
+++ src/url.js
@@ -1,2 +1,2 @@
-export const URL = "http://localhost:5000"
-export const IF = "http://localhost:5000/images/"
+export const URL = "https://adorable-mite-tux.cyclic.app"
+export const IF = "https://adorable-mite-tux.cyclic.app/images/"
